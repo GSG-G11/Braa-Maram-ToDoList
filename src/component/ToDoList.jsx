ca81313d148bacf6fb1455e3254a9667fd6e7d1c
Maram Ismail <maramismail2015@gmail.com>
2022-04-06 19:08:24 +0300

    create edit fun

--- src/component/ToDoList.jsx
+++ src/component/ToDoList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const ToDoList = ({ task , func ,complite}) => {
+const ToDoList = ({ task , func ,complite,updateTask}) => {
     //date
 //   let separator = "/";
 //   let newDate = new Date();
@@ -13,14 +13,18 @@ const ToDoList = ({ task , func ,complite}) => {
 //show task
   const tasklist = task.map((task,index) => {
     return (
-      <div key={task.id}>
+      <div key={task.id} className="container">
           <input type='checkbox' checked={task.isComplite} onChange={()=>complite(index)} />
         <span> {task.task}</span>
         <span>{task.date}</span>
         <i  onClick={()=>func(task.id)} className="fas fa-trash"></i>
+        <i  onClick={()=>updateTask(task.id)} className="fas fa-edit"></i>
       </div>
     );
   });
+
+  // new Date(taskEdit.date).toISOString();
+
  
   return <div>{tasklist}</div>;
 };
