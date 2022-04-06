62c9528a4db6a97f585d5f935f085eb972fd5f0b
braaAwni <braaberoo95@gmail.com>
2022-04-06 09:59:59 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import AddToDo from "./component/AddToDo";
+import ToDoList from "./component/ToDoList";
 
 
 function App() {
   return (
     <div className="App">
      <h3>to do list</h3>
+     <AddToDo />
+     <ToDoList />
     </div>
   );
+
+  
 }
 
 export default App;
