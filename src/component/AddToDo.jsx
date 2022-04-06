b3e0046d022104157e1ed07adf2873337be61fb3
Maram Ismail <maramismail2015@gmail.com>
2022-04-06 11:45:23 +0300

    create add component

--- src/component/AddToDo.jsx
+++ src/component/AddToDo.jsx
@@ -1,11 +1,30 @@
 import React,{Component} from 'react';
 
 class AddToDo extends Component {
-    
+  
+    state={
+        task:''
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+       this.props.func(this.state);
+       this.setState({task:''})
+    }
     render(){
+        const {task} = this.state;
+        
         return (
             <div>
-                
+                <form onSubmit={this.handleSubmit}>
+                    <input name="task" value ={task} type="text" onChange={this.handleChange} />
+                   <input type="submit" />
+                </form>
             </div>
         );
     }
