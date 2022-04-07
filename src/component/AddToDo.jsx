e1e50980b3d58cf3678dc9641cfb88707db4c4d3
braaAwni <braaberoo95@gmail.com>
2022-04-07 11:57:06 +0300

    fixed

--- src/component/AddToDo.jsx
+++ src/component/AddToDo.jsx
@@ -3,17 +3,16 @@ import React,{Component} from 'react';
 class AddToDo extends Component {
 
     render(){
-        const {handleChange,task,date,handleSubmit} = this.props
+        const {handleChange,task,date,handleSubmit,edit} = this.props
         return (
             <div className='to-do-form'>
                 <form onSubmit={handleSubmit}>
                     <input name="taskItem" value ={task} type="text" placeholder='your task' onChange={handleChange} required />
                     <input name="date" value ={date} type="text" placeholder='date' onChange={handleChange} required />
-                    <button type='submit'> Add</button>
+                    <button type='submit'> {edit?'Edit':'Add'}</button>
                 </form>
             </div>
         );
     }
 };
-// new Date(taskEdit.date).toISOString()
 export default AddToDo;
